bd669e8b43ee60b56ff547426e78f746febb864c
DeeCaaD <perttu.kangas@hotmail.fi>
2022-09-10 00:03:35 +0300

    Actually will leave this for tomorrow

--- src/main/kotlin/me/deecaad/weaponmechanicsplus/weapon/guidedprojectile/GuidedProjectile.kt
+++ src/main/kotlin/me/deecaad/weaponmechanicsplus/weapon/guidedprojectile/GuidedProjectile.kt
@@ -1,4 +1,23 @@
 package me.deecaad.weaponmechanicsplus.weapon.guidedprojectile
 
-class GuidedProjectile {
+import me.deecaad.core.file.SerializeData
+import me.deecaad.core.file.Serializer
+import me.deecaad.core.file.SerializerException
+
+class GuidedProjectile : Serializer<GuidedProjectile> {
+
+    /**
+     * Default constructor for serializer
+     */
+    constructor() {}
+
+    override fun getKeyword(): String {
+        return "Guided_Projectile"
+    }
+
+    @Throws(SerializerException::class)
+    override fun serialize(data: SerializeData): GuidedProjectile {
+
+        return GuidedProjectile();
+    }
 }
